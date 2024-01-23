d88adbb4019b74ffdd6aa4ad633e370a4252a9e8
Ayobamidel <152291627+Ayobamidel@users.noreply.github.com>
2024-01-23 14:29:01 +0100

    initial commit

---
+++ etlcustomers.py
@@ -0,0 +1,33 @@
+import requests #interact with APIs
+import pandas as pd #data manipulation
+from sqlalchemy import create_engine 
+import configparser 
+import urllib.parse
+
+
+def etl_process():
+    #load my credentials from my config
+    config = configparser.ConfigParser(interpolation=None)
+    config.read('config.ini')
+
+    # Create postgres engine
+    postgres_config = config['postgres']
+
+    # URL encode the password
+    encoded_password = urllib.parse.quote(postgres_config['password'], safe='')
+
+    engine = create_engine(
+        f"postgresql://{postgres_config['user']}:{encoded_password}@{postgres_config['host']}:{postgres_config['port']}/{postgres_config['database']}"
+    )
+
+# Northwind API request for Customers
+    url = 'https://demodata.grapecity.com/northwind/api/v1/Customers'
+    response = requests.get(url)
+    data = response.json()
+
+    # Load data into postgres
+    df = pd.json_normalize(data)
+    df.to_sql('customers_raw', engine, if_exists = 'replace', index= False)
+    engine.dispose()
+
+etl_process()
